134eeb68d56ae7f60dc1b4df4d1b9f96f7bef44f
unknown <hannelgwak@gmail.com>
2019-02-10 15:37:47 -0800

    questions to ask

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function luckyNumber(){
+    let randomNumber = Math.floor(Math.random()*1000)+1;
+    return randomNumber;
+}
+
+function greeting(userObject){
+    return(
+        <div className="container">
+            <h1>Hello, welcome {userObject.name}</h1>
+            <h2 className="text-muted">Your lucky number is {userObject.luckyNumber}</h2>
+        </div>
+    );
+}
+
+class User {
+    name='Sky';
+    luckyNumber=luckyNumber();
+}
+
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(new User),//why doesn't this need to be set to a new variable?, is es6 the reason why I didn't have to use this.name?
     document.getElementById('root')
 );

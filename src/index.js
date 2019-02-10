6f6ed34b3eb076f52eddd59c6bfc628b3e4d1d98
unknown <hannelgwak@gmail.com>
2019-02-10 15:04:45 -0800

    first prototype complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function luckyNumber(){
+    let randomNumber = Math.floor(Math.random()*1000)+1;
+    return randomNumber;
+}
+
+function greeting(userObject){
+    return(
+        <div className="container">
+            <h1>Hello, welcome {userObject.name}</h1>
+            <h2 className="text-muted">Your lucky number is {userObject.luckyNumber}</h2>
+        </div>
+    );
+}
+
+class User {
+    name='Sky';
+    luckyNumber=luckyNumber();
+}
+
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(new User),
     document.getElementById('root')
 );

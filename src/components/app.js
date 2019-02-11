5222fe39ac401de66ab3c7bd8bca34dd950e77ff
unknown <hannelgwak@gmail.com>
2019-02-10 16:16:48 -0800

    completed basic SGT

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,35 @@
+import React, {Component} from 'react';
+import Table from './table'
 
 
+const students = [
+    {
+        name: 'Sky',
+        course: 'Math',
+        grade:50
+    },
+    {
+        name: 'Brunce Banner',
+        course:'Anger Management',
+        grade:25
+    },
+    {
+        name: 'Clark Kent',
+        course: 'Journalism',
+        grade:100
+    }
+];
+
+
+class App extends Component{
+    render(){
+        return(
+            <div className="container">
+                <h1>Student Grade Table</h1>
+                <Table data={students} />
+            </div>
+        );
+    };
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function luckyNumber(){
-    let randomNumber = Math.floor(Math.random()*1000)+1;
-    return randomNumber;
-}
-
-function greeting(userObject){
-    return(
-        <div className="container">
-            <h1>Hello, welcome {userObject.name}</h1>
-            <h2 className="text-muted">Your lucky number is {userObject.luckyNumber}</h2>
-        </div>
-    );
-}
-
-class User {
-    name='Sky';
-    luckyNumber=luckyNumber();
-}
-
+import App from './components/app';
 
 ReactDOM.render(
-    greeting(new User),//why doesn't this need to be set to a new variable?, is es6 the reason why I didn't have to use this.name?
+    <App/>,
     document.getElementById('root')
 );
